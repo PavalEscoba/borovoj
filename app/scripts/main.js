1e69190717d1f1bb8a40e79e887fc3a281d19085
PavalEscoba <paval.escoba@gmail.com>
2017-03-13 21:59:37 +0300

    Added a responsive version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,33 @@
-$(document).ready(function(){	
+$(document).ready(function(){
+	
 	$('.bxslider').bxSlider({
 		auto: true,
 		pause: 7500,
 		controls: false,
 		pager: true
 	});
+	(function(){
+		$('.main-nav__toggle').on('click', function(e){
+			var navWrapper = $('.main-nav__wrapper'),
+				navToggle = $('.main-nav__toggle');
+
+				if(navToggle.hasClass('main-nav__toggle--closed')){
+					navWrapper
+						.removeClass('main-nav__wrapper--closed')
+						.addClass('main-nav__wrapper--opened');
+					navToggle
+						.removeClass('main-nav__toggle--closed')
+						.addClass('main-nav__toggle--opened');
+				}
+				else{
+					navWrapper
+						.removeClass('main-nav__wrapper--opened')
+						.addClass('main-nav__wrapper--closed');
+					navToggle
+						.removeClass('main-nav__toggle--opened')
+						.addClass('main-nav__toggle--closed');
+				}
+		})
+	}())
 
 });
-
